27e769cbdc9b2c7c2a52645926842863c1a421fa
MinhanTing <s4120167@student.rmit.edu.au>
2026-05-15 17:22:30 +1000

    Complete Assignment 2 submission-1

--- backend/shared/subscription_service.py
+++ backend/shared/subscription_service.py
@@ -1,42 +1,19 @@
 from boto3.dynamodb.conditions import Key
-
 from shared.dynamodb import subscription_table
-
-from shared.responses import (
-    success_response,
-    error_response,
-)
-
+from shared.responses import success_response, error_response
 
 def get_user_subscriptions(email):
-
     response = subscription_table.query(KeyConditionExpression=Key("email").eq(email))
-
     return response.get("Items", [])
 
-
 def subscribe_music(email, song):
-
-    existing = subscription_table.get_item(Key={"email": email, "title": song["title"]})
-
+    artist_title_year = song["artist"] + "#" + song["title"] + "#" + song["year"]
+    existing = subscription_table.get_item(Key={"email": email, "title": artist_title_year})
     if "Item" in existing:
         return error_response("subscription already exists")
-
-    subscription_table.put_item(
-        Item={
-            "email": email,
-            "title": song["title"],
-            "artist": song["artist"],
-            "year": song["year"],
-            "album": song["album"],
-            "img_url": song["img_url"],
-        }
-    )
-
+    subscription_table.put_item(Item={"email": email, "title": artist_title_year, "display_title": song["title"], "artist": song["artist"], "year": song["year"], "album": song["album"], "img_url": song.get("img_url", "")})
     return success_response(message="subscription added")
-
 
 def unsubscribe_music(email, title):
     subscription_table.delete_item(Key={"email": email, "title": title})
-
     return success_response(message="subscription removed")
